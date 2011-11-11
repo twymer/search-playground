2b7227c4b928e068892542fb3c515a273bc4c4cb
Tyler Wymer <twymer@gmail.com>
2011-11-10 20:52:10 -0500

    Large performance boosts
    Use heap to handle finding min f value.
    Pre-calculate neighbors of all points on map.

--- searching.py
+++ searching.py
@@ -1,9 +1,12 @@
 from collections import namedtuple
 from time import time
 import logging
+import heapq
 
 class Environment:
-  grid = []
+  def __init__(self):
+    self.grid = []
+    self.neighbors = {}
 
   def rows(self):
     return len(self.grid)
@@ -17,15 +20,20 @@ class Environment:
 
   # Determine if there is an obstacle blocking the way
   def passable(self, position):
-    if self.grid[position[0]][position[1]] != "%":
-      return True
-    else:
-      return False
+    return self.grid[position[0]][position[1]] != "%"
 
   def load_map(self, file_name):
     f = open(file_name, 'r')
     for line in f:
       self.grid.append(list(line))
+
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    for r in range(self.rows()):
+      for c in range(self.cols()):
+        self.neighbors[(r,c)] = [((r + d_row) % self.rows(), (c + d_col) % self.cols())
+          for (d_row, d_col) in directions]
+
+  
 
 class Search:
   def find_path(self, start_position, goal_position, environment):
@@ -40,13 +48,12 @@ class Search:
       d = 1 # movement cost
       return d * (abs(start[0] - goal[0]) + abs(start[1] - goal[1]))
 
-    def neighbors(pos):
-      directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-      return [((pos[0] + d_row) % environment.rows(), (pos[1] + d_col) % environment.cols())
-          for (d_row, d_col) in directions]
+    #def neighbors(pos):
+    #  directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    #  return [((pos[0] + d_row) % environment.rows(), (pos[1] + d_col) % environment.cols())
+    #      for (d_row, d_col) in directions]
 
     def trace_path(final_node):
-      print("trace")
       t = time()
       path = []
       current = final_node
@@ -58,20 +65,24 @@ class Search:
       return path
 
     open_nodes = {}
+    open_nodes_heap = []
     closed_nodes = {}
+    open_heap = []
 
     start_h = manhattan_distance(start_position, goal_position)
     current = Node(start_position, start_h, 0, start_h, None, 0)
     open_nodes[current.position] = current
+    heapq.heappush(open_nodes_heap, (current.f, current))
     while open_nodes:
       # Grab item with minimum F value
-      current = min(open_nodes.values(), key=lambda x:x.f)
-      if current.position == goal_position:
-        logging.error("steps to find path: " + str(current.depth))
-        return trace_path(current)
+      _, current = heapq.heappop(open_nodes_heap)
       del open_nodes[current.position]
+      #current = min(open_nodes.values(), key=lambda x:x.f)
+      if current.position == goal_position:
+        # logging.error("steps to find path: " + str(current.depth))
+        return trace_path(current)
       closed_nodes[current.position] = current
-      for neighbor in neighbors(current.position):
+      for neighbor in environment.neighbors[current.position]:
         if (environment.passable(neighbor) and # Add if passable..
             neighbor not in open_nodes and # and not open
             neighbor not in closed_nodes and # or closed
@@ -86,6 +97,7 @@ class Search:
             current,
             current.depth + 1)
           open_nodes[new.position] = new
+          heapq.heappush(open_nodes_heap, (new.f, new))
       # If first item has no neighbors, ant is trapped, put a noop on his move list
       if current.depth == 0 and len(open_nodes) == 0:
         return [current.position]
@@ -102,6 +114,7 @@ s = Search()
 e.load_map("big_maze.txt")
 # for i in range(e.cols()):
 #   for j in range(e.rows()):
+
 for i in range(20):
   for j in range(20):
     s.find_path((2,2), (i,j), e)
